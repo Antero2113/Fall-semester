b1e31b8ea3f029cb2279c7136b1c19958c00b45b
Anastasia Karandasheva <142301235+Antero2113@users.noreply.github.com>
2024-01-10 16:55:38 +0300

    Create WordsControllerTest.cs

---
+++ Tests-lab4/WordsControllerTest.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+using WebApplication1.Controllers;
+
+namespace WordsControllerTests
+{
+    [TestFixture]
+    public class WordsControllerTests
+    {
+        private WordsController _wordsController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _wordsController = new WordsController();
+        }
+
+        [Test]
+        public void AddWord_ReturnsOkResult()
+        {
+            var result = _wordsController.AddWord("test");
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+
+        [Test]
+        public void GetWordsJson_ReturnsString()
+        {
+            var result = _wordsController.GetWordsJson();
+            Assert.IsInstanceOf<string>(result.Value);
+        }
+
+        [Test]
+        public void SaveWordsJson_ReturnsOkResult()
+        {
+            var result = _wordsController.SaveWordsJson("test");
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+
+        [Test]
+        public void GetWordsXml_ReturnsString()
+        {
+            var result = _wordsController.GetWordsXml();
+            Assert.IsInstanceOf<string>(result.Value);
+        }
+
+        [Test]
+        public void SaveWordsXml_ReturnsOkResult()
+        {
+            var result = _wordsController.SaveWordsXml("test");
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+
+        [Test]
+        public void GetWordsFromSQLite_ReturnsList()
+        {
+            var result = _wordsController.GetWordsFromSQLite();
+            Assert.IsInstanceOf<List<string>>(result.Value);
+        }
+
+        [Test]
+        public void SaveWordsToSQLite_ReturnsOkResult()
+        {
+            var result = _wordsController.SaveWordsToSQLite();
+            Assert.IsInstanceOf<OkResult>(result);
+        }
+    }
+}
